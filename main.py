4193dcfa251533f600aa9299fc51a5729cdc1e7e
Mike DeLong <mikedelong@outlook.com>
2023-06-22 09:14:17 -0400

    feature: first working version to build the read frame

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from logging import getLogger
 from arrow import now
 from pandas import DataFrame
 from pandas import read_excel
+from pandas import concat
 
 
-def get_excel_data(sheet_name, url: str) -> DataFrame:
-    result_df = read_excel(io=url, sheet_name=sheet_name, )
+def get_baseline_data(sheet_name, url: str) -> DataFrame:
+    result_df = read_excel(io=url, sheet_name=sheet_name,
+                           dtype={'Class': str}, )
     return result_df
+
+
+def get_books_data(filename: str) -> DataFrame:
+    dfs = []
+    dtype = {'Dewey': str}
+    usecols = ['Date', 'Dewey', 'Author', 'Title']
+    for year in range(2017, 2024):
+        sheet_name = str(year) + ' Counts'
+        dfs.append(read_excel(dtype=dtype, engine='openpyxl', io=filename, sheet_name=sheet_name, usecols=usecols, ))
+    return concat(dfs)
 
 
 def load_settings(filename: str, ) -> dict:
@@ -28,17 +40,27 @@ def main():
     settings = load_settings(filename='main.json', )
     logger.info(msg='settings: {}'.format(dumps(indent=4, obj=settings, sort_keys=True, ), ), )
 
-    df = get_excel_data(sheet_name=settings['dewey_sheet_name'], url=settings['dewey_url'], )
+    df = get_baseline_data(sheet_name=settings['dewey_sheet_name'], url=settings['dewey_url'], )
     logger.info(msg=df.shape)
     df = df[df['Summary'] == 3].drop(columns=['Summary'])
     df = df[df['Caption'] != '[Unassigned]']
 
     DEBUG['data'] = df
 
+    read_df = get_books_data(filename=DATA_FOLDER + settings['books_read_file'])
+    read_df = read_df[~read_df['Dewey'].isna()]
+    read_df['short Dewey'] = read_df['Dewey'].apply(func=lambda x: str(x).split('.')[0])
+    read_df = read_df.sort_values(by=['short Dewey', 'Date'])
+    short_df = read_df.groupby(by='short Dewey').first()
+    logger.info(read_df.shape)
+    DEBUG['read'] = read_df
+    DEBUG['short'] = short_df
+
     time_seconds = (now() - time_start).total_seconds()
     logger.info(msg='done: {:02d}:{:05.2f}'.format(int(time_seconds // 60), time_seconds % 60, ))
 
 
+DATA_FOLDER = './data/'
 DEBUG = {}
 OUTPUT_FOLDER = './result/'
 USECOLS = []
